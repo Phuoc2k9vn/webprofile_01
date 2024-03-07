6a2f6e9c3db73110ac5e174ae072d26bc585162c
Phuoc2k9vn <79633431+Phuoc2k9vn@users.noreply.github.com>
2024-03-07 22:24:30 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBnjNDKnW9Fa6ccvIxoC_gITd1JxFVjQJY",
+    authDomain: "ipxevn.firebaseapp.com",
+    databaseURL: "https://ipxevn-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ipxevn",
+    storageBucket: "ipxevn.appspot.com",
+    messagingSenderId: "42379988966",
+    appId: "1:42379988966:web:2b20f00c8da3e4709bd027",
+    measurementId: "G-3X75G04KD4"
